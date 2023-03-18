cd2c24577facbd2eb0167a39e5b7c32e96372409
akovalski <126591997+AD0T@users.noreply.github.com>
2023-03-18 19:33:46 -0400

    Too Many Errors and Comments Update

--- 3x_plus_1.py
+++ 3x_plus_1.py
@@ -1,14 +1,29 @@
+## Import for .csv capabilities
 import csv
-
-def three():
-    number = int(input("Enter your number: "))
+          
+def equation():
+    ## Is it a Natural Number
+    naturalNumber = False
+    while naturalNumber == False:
+        try:
+            ## Accepting input
+            number = int(input("Enter your number: "))
+            ## Checking input
+            if number >= 1:
+                naturalNumber = True
+            else:
+                print("Please enter a natural number (A number larger than 0 without a decimal)")
+        except ValueError: 
+            print("Please enter a natural number (A number larger than 0 without a decimal)")
+    ## Setup .csv file
     f = open(f"answer.csv", 'w')
     writer = csv.writer(f)
     header = ['Step', 'Number']
     writer.writerow(header)
-    step = 0;
+    step = 0
     data =[step, number]
     writer.writerow(data)
+    ## Equation
     while number != 1:
         if number !=1:
             if number % 2:
@@ -17,9 +32,13 @@ def three():
             else:
                 number/=2
             step+=1
+            ## Finish .csv Step
             data =[step, number]
             writer.writerow(data)
+            ## Print step, number
             print(f"{step}: {number}")
+    ## Print the total
     print(f"It took {step} steps to get to 1")
 
-three()
+## Run the function
+equation()
